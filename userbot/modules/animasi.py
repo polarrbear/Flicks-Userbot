cf1c1ee3533537d422d59dfe1ded5af303c77146
PolarBear <96244695+polarrbear@users.noreply.github.com>
2022-04-21 19:43:23 +0700

    Update animasi.py

--- userbot/modules/animasi.py
+++ userbot/modules/animasi.py
@@ -3,15 +3,15 @@ from userbot import CMD_HELP, CMD_HANDLER as cmd
 from userbot.utils import flicks_cmd
 
 
-@flicks_cmd(pattern="sadboy(?: |$)(.*)")
+@flicks_cmd(pattern="apis(?: |$)(.*)")
 async def typewriter(typew):
     typew.pattern_match.group(1)
     sleep(2)
-    await typew.edit("`Pertama-tama kamu cantik`")
+    await typew.edit("`Haii Saya Apis`")
     sleep(2)
-    await typew.edit("`Kedua kamu manis`")
+    await typew.edit("`16 Tahun Kelas 2 SMAN`")
     sleep(1)
-    await typew.edit("`Dan yang terakhir adalah kamu bukan jodohku:)`")
+    await typew.edit("`Salam Kenal`")
 
 # Create by myself @localheart
 
